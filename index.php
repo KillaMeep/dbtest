6fff4e3d176ab584cd481032df8060e049911883
KillaMeep <killerpenguin729@gmail.com>
2024-05-19 07:45:33 -0600

    update index.php to have proper title

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Navbar in iFrame</title>
+    <title>ItemDB</title>
     <link href="assets/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
